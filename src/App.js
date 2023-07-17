4748c369dbe5240cd3beb04c42199576f1a799a5
Georgije024 <markovic.ra107.2018@uns.ac.rs>
2023-07-17 13:14:06 +0200

    03. Personal assistansts project for showcasing the use of components and props

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import 'bulma/css/bulma.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
+
+function App(){
+    return (
+        <div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title '>Personal Digital Assistants</p>
+                </div>
+            </section>
+            
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-3">
+                            <ProfileCard title="Alexa" 
+                            handle = "@alexa21"
+                             image= {AlexaImage}
+                             description ="Alexa was created by amazon "
+                             />
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard title = "Cortana" 
+                            handle = "@cortana32"
+                            image = {CortanaImage}
+                            description ="Cortana  was created by Microsoft "/>
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard title = "Siri" 
+                            handle ="@siri123" 
+                            image = {SiriImage}
+                            description ="Siri was created by Apple "
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>  
+        </div>
+    );
+}
+export default App;
