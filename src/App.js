e66d3ee16caf40ae64c267873abf5ae1c2b3fa37
Georgije024 <markovic.ra107.2018@uns.ac.rs>
2023-07-17 14:09:14 +0200

    Simple app that presents images of animals , showcasing the use of events and states

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { useState } from "react";
+import AnimalShown from "./AnimalShown";
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow' , 'dog' , 'dog' , 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
+function App () {
+
+    const [animals,setAnimals] = useState( [] );
+
+    const handleClick = () => {
+        setAnimals([...animals,getRandomAnimal()]);
+    };
+
+    const renderedAnimals = animals.map((animal , index) => {
+        return <AnimalShown type={animal} key={index} />;
+    });
+
+    return (
+        <div className='app'>
+            <button onClick={handleClick}>Add Animal</button>
+            <div className='animal-list'>{renderedAnimals}</div>
+        </div>
+    );
+}
+
+export default App;
